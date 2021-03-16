9b6461dff621eb8d5b42e049f2fa100731c32fb5
andityadimas1 <dimas.aninditya@xapiens.id>
2021-03-16 08:58:46 +0700

    tambah user seeder

--- models/seeder.go
+++ models/seeder.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"fmt"
-	"strconv"
 
 	"gorm.io/gorm"
 )
@@ -19,10 +18,48 @@ func SeederAddtask(db *gorm.DB) {
 
 	for _, data := range AddtaskArray {
 		// Get Data from Array
-		ID, _ := strconv.ParseInt(data[1], 10, 64)
-		TaskNama, _ := strconv.ParseInt(data[2], 10, 64)
+		task.ID = 0
+		task.TaskNama = data[0]
 		task.Completed = data[0]
 		db.Create(&task)
 	}
-	fmt.Println("Movie Data has been Seed!")
+	fmt.Println("seed!")
 }
+
+func SeederUser(db *gorm.DB) {
+	var userArray = [...][4]string{
+		{"DImmas.anin@xapiens.id", "saya123", "Dimas ganteng", "admin"},
+		{"sahlannasution@gmail.com", "inisahlan", "Sahlan ganteng", "guest"},
+		{"dimasdimas@gmail.com", "dimdim1234", "dimas", "guest"},
+	}
+
+	func SeederUser(db *gorm.DB) {
+		var userArray = [...]string{
+			"admin",
+			"test",
+			"guest",
+		}
+	
+		var user Users
+	
+		for _, v := range userArray {
+			user.Username = v
+			user.Role = v
+			user.Password = v
+	
+			// enkrip password
+			// param 1 dari password yang sudah ditentukan (contoh : admin)
+			// kita bikin konfersi dari string ke byte -> caranya []byte(user.Password)
+			encrypt, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	
+			// checking apakah proses enkripsi error / tidak
+			if err != nil {
+				log.Println(err)
+			}
+	
+			user.Password = string(encrypt)
+			user.ID = 0 // declare id dimulai dari 0, karena auto increment
+			db.Create(&user)
+		}
+		fmt.Println("Seeder user created")
+	}
